5a0aab609debc8b4b0df3c748c9d371b5640e72d
Hasitha Madushan Epa <88420265+Hasitha076@users.noreply.github.com>
2024-03-02 03:51:18 +0530

    Update apiSlice.js

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.baseURL }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://expense-tracker-backend-g7ho.onrender.com/" }),
     endpoints: builder => ({
         getCategories: builder.query({
             query: () => ({
